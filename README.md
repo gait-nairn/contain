0f594563b542e7e9be4c14869b921ec227e5ed41
Gait Nairn <gaitn11@gmail.com>
2021-04-07 16:28:53 -0700

    update unicode.py and README

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 
 Build status:
 
-1. [![](https://github.com/mikeizbicki/containers/workflows/tests-fibonacci/badge.svg)](https://github.com/mikeizbicki/containers/actions?query=workflow%3Atests-fibonacci)
-1. [![](https://github.com/mikeizbicki/containers/workflows/tests-range/badge.svg)](https://github.com/mikeizbicki/containers/actions?query=workflow%3Atests-range)
+1. [![](https://github.com/gait-nairn/contain/containers/workflows/tests-fibonacci/badge.svg)](https://github.com/mikeizbicki/containers/actions?query=workflow%3Atests-fibonacci)
+1. [![](https://github.com/gait-nairn/contain/containers/workflows/tests-range/badge.svg)](https://github.com/mikeizbicki/containers/actions?query=workflow%3Atests-range)
 <<<<<<< HEAD
 >>>>>>> 0134db5b825150255d40482e61190ede3506d9a0
 =======
-1. [![](https://github.com/mikeizbicki/containers/workflows/tests-unicode/badge.svg)](https://github.com/mikeizbicki/containers/actions?query=workflow%3Atests-unicode)
+1. [![](https://github.com/gait-nairn/contain/containers/workflows/tests-unicode/badge.svg)](https://github.com/mikeizbicki/containers/actions?query=workflow%3Atests-unicode)
 >>>>>>> b2d46c4950c56835211bb5d5d606c2feb8883262
--- containers/unicode.py
+++ containers/unicode.py
@@ -20,25 +20,30 @@ class NormalizedStr:
     '''
 
     def __init__(self, text, normal_form='NFC'):
-        pass
+        self.text = unicodedata.normalize(normal_form, text)
+        self.normal_form = normal_form
 
     def __repr__(self):
         '''
         The string returned by the __repr__ function should be valid python code
         that can be substituted directly into the python interpreter to reproduce an equivalent object.
         '''
+        return "NormalizedStr('" + str(self.text) + "', '" + str(self.normal_form) + "')"
+        
 
     def __str__(self):
         '''
         This functions converts the NormalizedStr into a regular string object.
         The output is similar, but not exactly the same, as the __repr__ function.
         '''
+        return str(self.text)
 
     def __len__(self):
         '''
         Returns the length of the string.
         The expression `len(a)` desugars to a.__len__().
         '''
+        return len(self.text)
 
     def __contains__(self, substr):
         '''
@@ -48,22 +53,27 @@ class NormalizedStr:
         HINT:
         You should normalize the `substr` variable to ensure that the comparison is done semantically and not syntactically.
         '''
+        norm = unicodedata.normalize(self.normal_form, substr)
+        return norm in self.text
 
     def __getitem__(self, index):
         '''
         Returns the character at position `index`.
         The expression `a[b]` desugars to `a.__getitem__(b)`.
         '''
+        return self.text[index]
 
     def lower(self):
         '''
         Returns a copy in the same normalized form, but lower case.
         '''
+        return str(self.text).lower()
 
     def upper(self):
         '''
         Returns a copy in the same normalized form, but upper case.
         '''
+        return str(self.text).upper()
 
     def __add__(self, b):
         '''
@@ -74,6 +84,8 @@ class NormalizedStr:
         The addition of two normalized strings is not guaranteed to stay normalized.
         Therefore, you must renormalize the strings after adding them together.
         '''
+        new_b = unicodedata.normalize(self.normal_form, self.text+str(b))
+        return NormalizedStr(new_b)
 
     def __iter__(self):
         '''
@@ -82,3 +94,18 @@ class NormalizedStr:
         You'll need to define your own iterator class with the appropriate magic methods,
         and return an instance of that class here.
         '''
+        return NormalizedStrIter(self.text)
+
+
+class NormalizedStrIter:
+    def __init__(self, text):
+        self.text = text
+        self.i = 0
+
+    def __next__(self):
+        if len(self.text) <= self.i:
+            raise StopIteration
+        else:
+            next_char = self.text[self.i]
+            self.i += 1
+            return next_char
